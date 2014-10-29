0b6fb9cf078b50af4578236a8cba3165488d6b05
Tom De Vylder <tom@penumbra.be>
2014-10-29 11:25:27 +0100

    Add initial rspec coverage

---
+++ spec/classes/consul_template_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe 'consul_template', :type => :class do
+  let (:facts) { debian_facts }
+  let (:pre_condition) { '$concat_basedir = "/tmp"' }
+
+  describe 'without parameters' do
+    it { should create_class('consul_template') }
+    it { should contain_class('consul_template::package') }
+    it { should contain_class('consul_template::config') }
+    it { should contain_class('consul_template::service') }
+  end
+
+  describe 'with parameter: consul_host' do
+    let (:params) { { :consul_host => '10.10.10.10' } }
+
+    it { should contain_concat__fragment('header').with(
+        'content' => /10.10.10.10/
+      )
+    }
+  end
+
+  describe 'with parameter: consul_port' do
+    let (:params) { { :consul_port => '1111' } }
+
+    it { should contain_concat__fragment('header').with(
+        'content' => /1111/
+      )
+    }
+  end
+end
+
