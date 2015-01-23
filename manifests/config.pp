7f9357ca9fcdd87798005537e56c65c4587a83f2
Graham Taylor <graham.x.taylor@capgemini.com>
2015-01-23 13:16:11 +0000

    Ensure startup config gets installed AFTER the package and not vice verse

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,6 +4,35 @@
 # Creates required directories and initial configuration file.
 #
 class consul_template::config {
+
+  if $consul_template::init_style {
+    case $consul_template::init_style {
+      'upstart' : {
+        file { '/etc/init/consul-template.conf':
+          mode    => '0444',
+          owner   => 'root',
+          group   => 'root',
+          content => template('consul_template/consul-template.upstart.erb'),
+        }
+        file { '/etc/init.d/consul-template':
+          ensure => link,
+          target => "/lib/init/upstart-job",
+          owner  => root,
+          group  => root,
+          mode   => 0755,
+        }
+      }
+      'sysv' : {
+        file { '/etc/init.d/consul-template':
+          mode    => '0555',
+          owner   => 'root',
+          group   => 'root',
+          content => template('consul_template/consul-template.sysv.erb')
+        }
+      }
+    }
+  }
+
   file { '/etc/consul-template/':
     ensure => 'directory',
     group  => '0',
